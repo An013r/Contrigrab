32a4f2950efe6a1d2bff3621d04797d39e057137
KylaMKV <61297267+An013r@users.noreply.github.com>
2023-03-10 21:04:39 +0000

    Working

--- src/gh_get.py
+++ src/gh_get.py
@@ -1,2 +1,20 @@
-import os, sys,
+import os, sys, requests
 
+def getcont(user, day="0", returnformalday=False):
+    page = requests.get("https://github.com/" + user).text
+    page = page[page.find("js-calendar-graph-svg"):page.find("</g>  </svg>")]
+    page = page.split("\n")
+    if day != "0":
+        loop = 0
+        for ln in page:
+            #print("okx2")
+            if day in ln:
+                cont = int(page[loop][page[loop].find("data-level=\"") + 12:page[loop].find("rx=") - 2])
+                if returnformalday == False:
+                    return cont
+                else:
+                    formalday = ln[ln.find("contributions on ") + 17:ln.find("</rect>")]
+                    print(formalday)
+                    return cont, formalday
+            else:
+                loop += 1
--- src/main.py
+++ src/main.py
@@ -3,12 +3,25 @@ import gh_get as gh
 
 if len(sys.argv) <= 1:
     print("Not Enough Arguments")
-    print("contrigrab {{user}} [day]")
+    print("contrigrab {{user}} [day]\n\nday must be in mm-dd format(ex: 02-21 for February 21st)\nif a day is not specified, it will print it in the style that github presents it")
     exit(1)
+fnd = 0
 loop = 0
 user, arg = "", ""
+# TODO: Add argument to print it out in an easily parsable way, eg CONT=0;DAY="03-06" etc.
 for arg in sys.argv:
-    if loop == 0 and arg[0] != "-":
+    if loop == 0:
+        loop += 1
+        continue
+    if fnd == 0 and arg[0] != "-":
         user = arg
-    elif loop == 1 and arg[0] != "-":
+        fnd += 1
+    elif fnd == 1 and arg[0] != "-":
         day = arg
+if day == "":
+    # TODO: Implement this
+    NUL
+else:
+    #print("Contributions on day ")
+    cont, fday = gh.getcont(user, day=day, returnformalday=True)
+    print("User " + user + " has contributed " + str(cont) + " times on " + fday)
