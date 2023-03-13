272a3db76fd542fcd3353c4a24a0408964bd630a
KylaMKV <61297267+An013r@users.noreply.github.com>
2023-03-13 17:21:32 +0000

    Several changes

--- src/gh_get.py
+++ src/gh_get.py
@@ -2,19 +2,19 @@ import os, sys, requests
 
 def getcont(user, day="0", returnformalday=False):
     page = requests.get("https://github.com/" + user).text
+    # Removes everything in the HTML file thats not needed 
     page = page[page.find("js-calendar-graph-svg"):page.find("</g>  </svg>")]
     page = page.split("\n")
+
     if day != "0":
         loop = 0
         for ln in page:
-            #print("okx2")
             if day in ln:
-                cont = int(page[loop][page[loop].find("data-level=\"") + 12:page[loop].find("rx=") - 2])
+                cont = int(ln[ln.find("data-level=\"") + 12:ln.find("rx=") - 2])
                 if returnformalday == False:
                     return cont
                 else:
                     formalday = ln[ln.find("contributions on ") + 17:ln.find("</rect>")]
-                    print(formalday)
                     return cont, formalday
             else:
                 loop += 1
--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@ import gh_get as gh
 
 if len(sys.argv) <= 1:
     print("Not Enough Arguments")
-    print("contrigrab {{user}} [day]\n\nday must be in mm-dd format(ex: 02-21 for February 21st)\nif a day is not specified, it will print it in the style that github presents it")
+    print("contrigrab {user} {day}\n\nday must be in mm-dd format(ex: 02-21 for February 21st) or yy-mm-dd(ex: 23-02-21 for February 21st, 2023), this won't never work for the years before the last year, only this year and the previous year will work")
     exit(1)
 fnd = 0
 loop = 0
-user, arg = "", ""
-# TODO: Add argument to print it out in an easily parsable way, eg CONT=0;DAY="03-06" etc.
+user, day = "", ""
+parsable = False
 for arg in sys.argv:
     if loop == 0:
         loop += 1
@@ -18,10 +18,12 @@ for arg in sys.argv:
         fnd += 1
     elif fnd == 1 and arg[0] != "-":
         day = arg
-if day == "":
-    # TODO: Implement this
-    NUL
+    elif arg == "-P" or arg == "--parse":
+        parsable = True
 else:
-    #print("Contributions on day ")
-    cont, fday = gh.getcont(user, day=day, returnformalday=True)
-    print("User " + user + " has contributed " + str(cont) + " times on " + fday)
+    if not parsable:
+        cont, fday = gh.getcont(user, day=day, returnformalday=True)
+        print("User " + user + " has contributed " + str(cont) + " times on " + fday)
+    elif parsable:
+        cont, fday = gh.getcont(user, day=day, returnformalday=True)
+        print(str(cont))
