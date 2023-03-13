09d6bff3313e522973dc1b1bb546506b2a852c67
KylaMKV <61297267+An013r@users.noreply.github.com>
2023-03-13 17:37:08 +0000

    Made getcont() slightly easier to read

--- src/gh_get.py
+++ src/gh_get.py
@@ -1,20 +1,20 @@
 import os, sys, requests
 
-def getcont(user, day="0", returnformalday=False):
+def getcont(user, day, returnformalday=False):
     page = requests.get("https://github.com/" + user).text
     # Removes everything in the HTML file thats not needed 
     page = page[page.find("js-calendar-graph-svg"):page.find("</g>  </svg>")]
     page = page.split("\n")
 
-    if day != "0":
-        loop = 0
-        for ln in page:
-            if day in ln:
-                cont = int(ln[ln.find("data-level=\"") + 12:ln.find("rx=") - 2])
-                if returnformalday == False:
-                    return cont
-                else:
-                    formalday = ln[ln.find("contributions on ") + 17:ln.find("</rect>")]
-                    return cont, formalday
-            else:
-                loop += 1
+    for ln in page:
+        if not day in ln:
+            continue
+
+        cont = int(ln[ln.find("data-level=\"") + 12:ln.find("rx=") - 2])
+        if returnformalday == False:
+            return cont
+
+        else:
+            formalday = ln[ln.find("contributions on ") + 17:ln.find("</rect>")]
+            return cont, formalday
+
